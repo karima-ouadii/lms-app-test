1bf123dd26fab68ce46dc11d6477ae7c5e1c99d4
unknown <karima.ouadii18@gmail.com>
2021-03-10 12:42:14 +0100

    change

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 //la partie provider qui de repete pour chaque requette http
 export default axios.create({
-    baseURL:''
+    baseURL:'https://lms-projet-default-rtdb.firebaseio.com/'
 })
